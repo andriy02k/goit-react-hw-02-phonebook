5ccce12a5b19a76ab01bb99082c3a073b795890a
andriy02k <krainyk02@gmail.com>
2023-12-06 23:02:52 +0200

    fixed styles

--- src/components/List/List.styled.js
+++ src/components/List/List.styled.js
@@ -10,7 +10,12 @@ export const Lists = styled.ul(() => {
                 backgroundColor: 'white',
                 border: '1px solid black',
                 borderRadius: '4px',
-                marginLeft: '12px'
+                marginLeft: '12px',
+                '&:hover': {
+                    backgroundColor: 'rgb(23, 70, 164)',
+                    border: '1px solid white',
+                    boxShadow: '5px 5px 10px rgba(0, 0, 0, 0.1)',
+                }
             }
         }
     };
